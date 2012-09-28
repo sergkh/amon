9304584359fc5591ffe29e9970198e8124d7b764
Yaric <poison766@i.ua>
2012-09-28 22:48:42 +0300

    This is simple test, which explain me how to work

---
+++ src/ua/vntu/amon/monitor/ForExample.java
@@ -0,0 +1,20 @@
+package ua.vntu.amon.monitor;
+/**
+ * 
+ * @author Yaric
+ *
+ */
+
+
+public class ForExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("this is test");
+
+	}
+
+}
